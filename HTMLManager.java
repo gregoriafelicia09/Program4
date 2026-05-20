c6852a155ac8080258af8551943d98855bbaafc4
JesseH0630 <zxh0630@outlook.com>
2026-05-20 11:12:02 -0700

    Added constructor

--- HTMLManager.java
+++ HTMLManager.java
@@ -3,4 +3,12 @@ import java.util.*;
 public class HTMLManager {
 	private Queue<HTMLTag> tags;
 
+	public HTMLManager(Queue<HTMLTag> tags){
+		if(tags == null){
+			throw new IllegalArgumentException();
+		}
+		else{
+			this.tags = tags;
+		}
+	}
 }
